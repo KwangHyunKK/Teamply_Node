3ac7f5a2fbff1079eb7991a397f22cf681fdf302
Ppangg <panghyun35@naver.com>
2023-01-30 05:30:39 +0900

    db에 연결할 수 있는 module이 있는 파일

---
+++ db.js
@@ -0,0 +1,14 @@
+var mysql = require('mysql2/promise');
+var db = mysql.createPool({
+    host: process.env.mysql_host,
+    port: process.env.mysql_port,
+    user: process.env.mysql_user,
+    password: process.env.mysql_password,
+    database: process.env.mysql_database,
+    connectionLimit: 10,
+    connectTimeout: 10000
+});
+
+module.exports = db;
+
+// database access
