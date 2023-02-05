3ed70b7477eeb9232fad10f62934136476c1489a
Ppangg <panghyun35@naver.com>
2023-02-06 06:49:53 +0900

    refresh.js : 토큰 확인이 안 되었는데 구조 변경

--- refresh.js
+++ refresh.js
@@ -1,6 +1,6 @@
 const { sign, verify, refreshVerify } = require('./jwt-util');
 const jwt = require('jsonwebtoken');
-
+const mysql = require('mysql');
 
 const refresh = async (req, res) => {
     // access token과 refresh token의 존재 유무를 체크합니다.
@@ -12,21 +12,25 @@ const refresh = async (req, res) => {
       // access token 검증 -> expired여야 함.
       const authResult = verify(auth);
 
-      if(authResult.ok == false)
+      // bad access token
+      if(authResult.ok == false || authResult == null)
       {
-        return res.status(400).send({
-          ok: false,
-          message: 'Access token and refresh token are need for refresh!',
+        return res.status(401).send({
+          isSuccess: false,
+          code: 401,
+          message: 'No authorized',
         });
       }
-
+      
+      console.log(authResult);
       // access token 디코딩하여 user의 정보를 가져옵니다.
       const decoded = jwt.decode(auth);
 
       // 디코딩 결과가 없으면 권한이 없음을 응답.
       if (decoded === null) {
         res.status(401).send({
-          ok: false,
+          isSuccess: false,
+          code: 401,
           message: 'No authorized!',
         });
       }
@@ -40,15 +44,23 @@ const refresh = async (req, res) => {
         // 1. access token이 만료되고, refresh token도 만료 된 경우 => 새로 로그인해야합니다.
         if (refreshResult.ok === false) {
           res.status(401).send({
-            ok: false,
+            isSuccess: false,
+            code: 401,
             message: 'No authorized!',
           });
         } else {
           // 2. access token이 만료되고, refresh token은 만료되지 않은 경우 => 새로운 access token을 발급
+
+          user = {
+            id: result.id, // token이 검증되었으면 req에 값을 세팅하고, 다음 콜백함수로 갑니다.
+            hash: result.hash,
+            ip: result.ip
+          }
           const newAccessToken = sign(user);
   
           res.status(200).send({ // 새로 발급한 access token과 원래 있던 refresh token 모두 클라이언트에게 반환합니다.
-            ok: true,
+            isSuccess: true,
+            code: 200,
             data: {
               accessToken: newAccessToken,
               refreshToken,
@@ -58,14 +70,16 @@ const refresh = async (req, res) => {
       } else {
         // 3. access token이 만료되지 않은경우 => refresh 할 필요가 없습니다.
         res.status(400).send({
-          ok: false,
+          isSuccess: false,
+          code: 400,
           message: 'Acess token is not expired!',
         });
       }
     } else { // access token 또는 refresh token이 헤더에 없는 경우
-      res.status(400).send({
-        ok: false,
-        message: 'Access token and refresh token are need for refresh!',
+      res.status(401).send({
+        isSuccess: false,
+        code: 401,
+        message: 'No authorized!',
       });
     }
   };
