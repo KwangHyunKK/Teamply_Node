456ee5176c42b34f557b7737b7e2a06c430b5824
Ppangg <panghyun35@naver.com>
2023-02-14 05:16:28 +0900

    nodemailer 수정 : sendPDFMail -> sendEMail 수정, activateUser, DifferentIP, pwCode에 해당하는 문자열 출력 함수 생성

--- node_mailing/nodeMailing.js
+++ node_mailing/nodeMailing.js
@@ -10,7 +10,7 @@ function emailContent(subject, html){
 }
 
 //메일 보내기
-async function sendPDFMail(senderName,senderEmail,senderPass,senderSmtp,senderPort,receivers, emailContent){
+async function sendEMail(senderName,senderEmail,senderPass,senderSmtp,senderPort,receivers, emailContent){
 
     // 수신자에게 메일 전송
     let transporter = nodemailer.createTransport({
@@ -44,25 +44,35 @@ async function sendPDFMail(senderName,senderEmail,senderPass,senderSmtp,senderPo
 }
 
 //예시
-activateUser = (value) =>{return '[Teamply] Activate user',
-     `<b>안녕하세요, 00님.</b> <br/>
-        <b>아래의 [인증완료]버튼을 클릭하시면 이메일 인증이 되며 팀플리 회원가입이 완료됩니다.</b> <br/>
-        <b>또는 아래의 ` + value + `를 입력하시면 이메일 인증이 되며 팀플리 회원가입이 완료됩니다.</b> <br/>
+activateUser = (user, value) =>{
+    return {
+        "emailSubject": '[Teamply] Activate user',
+        "emailHtml": `<b>안녕하세요, ${user}님.</b> <br/>
+        <b>다음의 ${value}를 입력하시면 이메일 인증이 되며 팀플리 회원가입이 완료됩니다.</b> <br/>
         <b>팀플리와 함께 즐거운 팀플되세요:) </b>`
+    }
 }
 
-DifferentIP = (value1, value2) =>{return '[Teamply] Different IP Login',
-    `<b>안녕하세요, 00님.</b> <br/>
-        <b> Your login log is ` + value1 + `</br>
-        <b> However, you signed up at ` + value2 + `</br>
+DifferentIP = (user, value1, value2) =>{
+    return {
+        emaliSubject: '[Teamply] Different IP Login',
+        emailHtml: `<b>안녕하세요, ${user}님.</b> <br/>
+        <b>다른 IP에서 접속이 확인되었습니다.</b>
+        <b>기존 IP :  ${value1}</br>
+        <b> 최근 로그인 IP : ${value2}</br>
         <b>팀플리와 함께 즐거운 팀플되세요:) </b>`
+    }
 }
 
-passwordCode = (value1) =>{return '[Teamply] Different IP Login',
-    `<b>안녕하세요, 00님.</b> <br/>
-    <b>Your passwordCode is ${value1}</b>
-       <b>팀플리와 함께 즐거운 팀플되세요:) </b>`
+passwordCode = (user, value1) =>{
+    return {
+        emailSubject: '[Teamply] 비밀번호 인증 코드입니다',
+        emailHtml: `<b>안녕하세요, ${user}님.</b> <br/>
+        <b>회원님의 비밀번호 인증 코드는 ${value1}입니다.</b>
+        <b>팀플리와 함께 즐거운 팀플되세요:) </b>`
+    }
 }
+
 //예시
 emailContent('[Teamply] 이메일 인증을 진행해주세요.',
 `<b>안녕하세요, 00님.</b> <br/>
@@ -72,4 +82,4 @@ emailContent('[Teamply] 이메일 인증을 진행해주세요.',
 
 // sendPDFMail('팀플리',process.env.senderMail,process.env.senderPass,process.env.senderSmtp,process.env.Port,'example1@gmail.com', emailContent);
 
-module.exports = {sendPDFMail, emailContent, passwordCode, DifferentIP, activateUser};
+module.exports = {sendEMail, emailContent, passwordCode, DifferentIP, activateUser};
