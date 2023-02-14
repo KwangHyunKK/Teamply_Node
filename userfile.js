8b5fa984d6d1cdab60f554613f7c46c664c237d5
Ppangg <panghyun35@naver.com>
2023-02-14 18:47:09 +0900

    fileupdate.js 파일 추가함

--- userfile.js
+++ userfile.js
@@ -16,14 +16,25 @@ var db = mysql.createPool({
 router.post('/img', authJWT, upload.single('file'), async(req, res, next)=>{
     let conn = null;
     try{
-        // const query = `insert `
-        console.log(req.file.originalname);
+        const str = req.file.originalname.split('.');
+        const query1 = `delete from PhotoFile where user_id = ${req.user_id}`;
+        const query2 = `insert into PhotoFile(user_id, file_type, file_name) Values(${req.user_id}, ${str[1]}, ${req.file.originalname});`;
+        conn = await db.getConnection();
+        await conn.beginTransaction();
+        await conn.query(query1);
+        await conn.query(query2);
+        await conn.commit();
+        conn.release();
         return res.status(200).send({
             isSuccess: true,
             statuscode: 200,
             message: '파일 업로드 성공!',
         });
     }catch(err){
+        if(conn!=null){
+            await conn.rollback();
+            conn.release();
+        }
         return res.statusCode(400).send({
             isSuccess: false,
             statuscode:400,
@@ -32,16 +43,23 @@ router.post('/img', authJWT, upload.single('file'), async(req, res, next)=>{
     }
 })
 
-router.get('/img', authJWT, (req, res, next)=>{
+router.get('/img/:userid', authJWT, async(req, res, next)=>{
     let conn = null;
     try{
-
+        const query = `select file_name from PhotoFile where userid = ${req.params.userid}`;
+        conn = await db.getConnection();
+        const [result] = await conn.query(query);
+        conn.release();
+        const stream = fs.createReadStream(`./uploads/${result[0].file_name}`);
+        res.setHeader('Content-Disposition', `attachment; filename=${result[0].file_name}`);
+        stream.pipe(res);
     }catch(err){
-        
-    }
-    const stream = fs.createReadStream(`./uploads/${req.params.filename}`);
-    res.setHeader('Content-Disposition', `attachment; filename=${req.params.filename}`);
-    stream.pipe(res);
+        return res.statusCode(400).send({
+            isSuccess: false,
+            statuscode:400,
+            message: '파일 다운로드 실패!',
+        });
+    };
 })
 
 module.exports = router;
